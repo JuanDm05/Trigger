a35a5092263fce06ed82c6f96580732927084c13
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2024-01-16 08:59:23 -0700

    Agregar archivos de proyecto.

---
+++ Trigger/Trigger/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Trigger
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public class ChangeColor : TriggerAction<Button>
+        {
+            protected override void Invoke(Button btn)
+            {
+                btn.BackgroundColor = Color.DarkOrange;
+                btn.Text = "Here we go!!!";
+            }
+        }
+    }
+}
+
